11c02d22df43fd02b165083c19cfc0c6be4d23f6
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2016-05-13 07:57:39 +0200

    Make ZCA standalone, not needing $fpath update

--- zsh-cmd-architect.plugin.zsh
+++ zsh-cmd-architect.plugin.zsh
@@ -1,7 +1,24 @@
 #!/usr/bin/env zsh
 
+#
+# No plugin manager is needed to use this file. All that is needed is adding:
+#   source {where-znt-is}/zsh-cmd-architect.plugin.zsh
+#
+# to ~/.zshrc.
+#
+
 REPO_DIR="${0%/*}"
 CONFIG_DIR="$HOME/.config/zca"
+
+#
+# Update FPATH if:
+# 1. Not loading with Zplugin
+# 2. Not having fpath already updated (that would equal: using other plugin manager)
+#
+
+if [[ -z "$ZPLG_CUR_PLUGIN" && "${fpath[(r)$REPO_DIR]}" != $REPO_DIR ]]; then
+    fpath+=( "$REPO_DIR" )
+fi
 
 #
 # Copy configs
